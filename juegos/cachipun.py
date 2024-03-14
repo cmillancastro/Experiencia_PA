398538118ddc3ba8c0101edd8db2597b31815d36
cmillancastro <163418271+cmillancastro@users.noreply.github.com>
2024-03-14 21:39:48 +0000

    Ahora hice el juego de cachipun

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,31 @@
 def cachipun():
-    """
-    Esta función representa el juego de cachipun.
-    Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
-    La computadora debe elegir una opción al azar.
-    """
+    from random import randint
+    azar = randint(1,3)
+    jugador = input("Elige entre piedra, papel o tijera: ")
+    jugador = jugador.lower()
+    if azar == 1:
+        computadora = "piedra"
+    elif azar == 2:
+        computadora = "papel"
+    elif azar == 3:
+        computadora = "tijera"
+    if jugador == computadora:
+        print("Han empatado! Intentálo de nuevo...")
+        return cachipun()
+    elif jugador != computadora:
+        if jugador == "tijera":
+            if computadora == "piedra":
+                print("Has perdido contra la piedra :(")
+            elif computadora == "papel":
+                print("Has ganado contra el papel !!")
+        elif jugador == "piedra":
+            if computadora == "tijera":
+                print("Has ganado contra la tijera !!")
+            elif computadora == "papel":
+                print("Has perdi1do contra el papel :(")
+        elif jugador == "papel":
+            if computadora == "tijera":
+                print("Has perdido contra la tijera :(")
+            elif computadora == "piedra":
+                print("Has ganado contra la piedra !!")
     pass
