1843ecd528bf3364dcbc81b8054e36ac07d48b7f
cmillancastro <163418271+cmillancastro@users.noreply.github.com>
2024-03-14 23:41:09 +0000

    Juego par o impar

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,3 +1,16 @@
 def adivinar_par_o_impar():
-    
+    from random import randint
+    paridad = input("¿El numero es par o impar?: ")
+    paridad = lower.paridad()
+    numeroaleatorio = randint(0, 100)
+    if numeroaleatorio%2 == 0:
+        if paridad == "par":
+            print("Has acertado!! El número " + str(numeroaleatorio) + "  es par")
+        else:
+            print("No has acertado :( " + str(numeroaleatorio) + " no es impar")
+    elif numeroaleatorio%2 != 0:
+        if paridad == "par":
+            print("No has acertado :( " + str(numeroaleatorio) + " no es par")
+        else:
+            print("Has acertado!! El número " + str(numeroaleatorio) + "  es impar")
     pass
