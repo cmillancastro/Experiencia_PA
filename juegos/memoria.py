ea90946d7f30c47d3120da6b8b108f5dc9b8c414
cmillancastro <163418271+cmillancastro@users.noreply.github.com>
2024-03-14 23:35:33 +0000

    No lo logré hacer

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,4 +1,5 @@
 def memoria():
+    "Esta función representa el juego de memoria. Debes generar una secuencia de números al azar y mostrarla al usuario. Luego, debes pedir al usuario que repita la secuencia. Se debe mostrar un mensaje si el usuario acierta o no."
     from random import randint
     numero = ""
     for i in range(4):
