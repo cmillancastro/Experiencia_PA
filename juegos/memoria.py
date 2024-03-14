b68a473bf2128c4a02c24893ab9f18c2ff566f0c
cmillancastro <163418271+cmillancastro@users.noreply.github.com>
2024-03-14 23:27:02 +0000

    Intento 1 juego memoria

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,15 @@
 def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
+    from random import randint
+    numero = ""
+    for i in range(4):
+        digito = randint(0,9)
+        numero += str(digito)
+    string = "Intenta recordar este número: " + numero
+    print(string)
+    intento = input("¿Cuál era el número?: ")
+    if intento == numero:
+        print("Has acertado !!")
+    elif intento != numero:
+        print("No lo has logrado :(, el número era " + numero)
+
     pass
