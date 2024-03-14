be86590301ed738cec40bc7040a61e9b7a2f429e
cmillancastro <163418271+cmillancastro@users.noreply.github.com>
2024-03-14 21:47:39 +0000

    Sorry todos los cambios

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,10 +1,11 @@
 def adivinar_numero():
     from random import randint
     numero = randint(1,10)
-    intento = input("Adivina el número: ")
+    intento = input("Adivina el número (elige entre el 1 y el 10): ")
     if numero == intento:
         print("Has acertado!!!")
     elif numero != intento:
-        string = "No has acertado :(, el número era" , str(numero)
+        strnumero = str(numero)
+        string = "No has acertado :(, el número era " + str(numero)
         print(string)
     pass
