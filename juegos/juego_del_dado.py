71dd1887613f9f80c4d6a6b0608e454b2e2f87d7
ConstanzaZamorano <163418541+ConstanzaZamorano@users.noreply.github.com>
2024-03-15 01:25:47 +0000

    juego del dado

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -5,4 +5,24 @@ def juego_del_dado():
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
-    pass
+    import random
+    lanzar = input('presiones enter para jugar')
+    ptos_jugador = 0
+    ptos_com = 0
+    while ptos_jugador < 30 and ptos_com < 30:
+        input('presione enter para jugar')
+        num = random.randint(1,6)
+        ptos_jugador += num
+        print('jugador posee', ptos_jugador, 'puntos')
+        if ptos_jugador < 30:
+            num_com = random.randint(1,6)
+            ptos_com += num_com
+            print('computador posee', ptos_com, 'puntos')
+            if ptos_com >= 30:
+                print('ha ganado computador')
+        else:
+            print('ha ganado jugador')
+
+juego_del_dado()
+
+
